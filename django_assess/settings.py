3c33ceb4c38a02bd8d1e05c9085bdd065f89664e
Gajraj Singh Chouhan <gajrajgchouhan@gmail.com>
2021-11-18 01:31:20 +0530

    update

--- django_assess/settings.py
+++ django_assess/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-@6+d_ymftdf^qj4jk^yw_y&766!+jnfdq=bgvodgq=6w%3o)7l"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "readingright-grocery-gajraj.herokuapp.com", "0.0.0.0"]
 
